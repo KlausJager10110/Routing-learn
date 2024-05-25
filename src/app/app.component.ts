d9ede0224a086e051fb144156331ac5afead1595
Chorn <pacharapolpacharapol2547@gmail.com>
2024-05-25 23:04:46 +0700

    final ep.1 Angilar Router learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// import { HomeComponent } from './home/home.component';
+// import { AboutComponent } from './about/about.component';
+// import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    // HomeComponent,
+    // AboutComponent,
+    // ContactComponent, // มีการ imported ไปใช้ในตัว app.routes.ts แล้ว
+    NavbarComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        'path': 'home',
+        'title': 'Home', // เปลี่ยน title ของ page นั้นๆ
+        component: HomeComponent,
+    },
+    {
+        'path': 'about',
+        'title': 'About', // เปลี่ยน title ของ page นั้นๆ
+        component: AboutComponent,
+    },
+    {
+        'path': 'contact',
+        'title': 'Contact', // เปลี่ยน title ของ page นั้นๆ
+        component: ContactComponent,
+    },
+    {
+        'path': '',
+        redirectTo: 'home', // ทำให้พอลบ path ที่อยู่ลังจาก http://localhost:4200 ออก จะ redirect ไปที่หน้า home
+        pathMatch: 'full',
+    },
+    {
+        'path': '**', // ใช้หรับหรับ path อื่นนอกเหนือจากที่ประกาศไว้
+        component: PageNotFoundComponent,
+    },
+];
